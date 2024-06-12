9844b8c45d1bb8bbbc92178055f87622d0a1e29d
Xavier Bizoux <xavier.bizoux@sas.com>
2024-06-12 07:46:27 +0200

    Add dynamic pages

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -13,18 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-from django.conf.urls.static import static
+
 from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-    path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
+    path("admin/", admin.site.urls),
+    path("about/", TemplateView.as_view(template_name="About.html")),
+    path("contact/", TemplateView.as_view(template_name="Contact.html")),
+    path("login/", TemplateView.as_view(template_name="index.html")),
+    path("register/", TemplateView.as_view(template_name="index.html")),
+    path("dealers/", TemplateView.as_view(template_name="index.html")),
+    path("dealer/<int:dealer_id>", TemplateView.as_view(template_name="index.html")),
+    path(
+        "postreview/<int:dealer_id>", TemplateView.as_view(template_name="index.html")
+    ),
+    path("djangoapp/", include("djangoapp.urls")),
+    path("", TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,13 +1,34 @@
-import LoginPanel from "./components/Login/Login"; 
-import Register from "./components/Register/Register";
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom'
+import Dealer from './components/Dealers/Dealer'
+import Dealers from './components/Dealers/Dealers'
+import LoginPanel from './components/Login/Login'
+import Register from './components/Register/Register'
+import PostReview from './components/Dealers/PostReview'
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route
+                path='/login'
+                element={<LoginPanel />}
+            />
+            <Route
+                path='/register'
+                element={<Register />}
+            />
+            <Route
+                path='/dealers'
+                element={<Dealers />}
+            />
+            <Route
+                path='/dealer/:id'
+                element={<Dealer />}
+            />
+            <Route
+                path='/postreview/:id'
+                element={<PostReview />}
+            />
+        </Routes>
+    )
 }
-export default App;
+export default App
